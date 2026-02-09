59cbae4c8736ff3f18c8be40469625a959ee5ad0
Ashish Pratap Singh <ashishpratap.singh@outlook.com>
2026-02-09 23:23:20 +0530

    hi

--- calc.c
+++ calc.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 int main(int argc, char const *argv[])
 {
-    char op='\0';
     double num1=0.0;
     double num2=0.0;
     double result=0.0;
     printf("Enter num1: ");
     scanf("%lf",&num1);
+
     printf("Enter operator: ");
     scanf(" %c",&op);//input buffer
     printf("Enter num2: ");
