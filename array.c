4e31f65759c82cf52a9189b7f014bfce0723db86
Ashish Pratap Singh <ashishpratap.singh@outlook.com>
2026-03-13 15:29:15 +0530

    Add binary search implementation and output messages for target found/not found

---
+++ array.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    int arr[14] = {2, 4, 6, 8, 12, 20, 25, 40, 45, 90, 108, 120, 140, 150};
+    int l = 0;
+    int r = 13;  // Could use sizeof(arr)/sizeof(arr[0]) - 1 for flexibility
+    int target = 108;
+    int found = 0;  // Flag to track if target is found
+
+    while (l <= r) {
+        int mid = (l + r) / 2;
+        if (arr[mid] == target) {
+            printf("Target found\n");
+            found = 1;
+            break;
+        } else if (arr[mid] > target) {
+            r = mid - 1;
+        } else {  // Fixed: This should be the case when arr[mid] < target
+            l = mid + 1;
+        }
+    }
+
+    if (!found) {
+        printf("Target not found\n");
+    }
+
+    return 0;
+}
---
+++ array2.c
@@ -0,0 +1,26 @@
+// #include <stdio.h>
+// int main(int argc, char const *argv[])
+// {
+//     int arr[10]={1,2,3,4,5};
+//     int sum=0;
+//     for(int i=0;i<=4;i++){
+//         sum=sum+arr[i];
+//     }
+//     printf("%d",sum);
+//     return 0;
+// }
+//-------------------------------------------------
+//  #include <stdio.h>
+//  int main(int argc, char const *argv[])
+//  {
+//     int arr[10]={1,2,3,5,4555};
+//     int n=10;
+//     int temp=arr[0];
+//     for(int i=0;i<n;i++){
+//         if (temp<arr[i]){
+//             temp=arr[i];
+//         }
+//     }
+//     printf("%d",temp);
+//  }
+//---------------------------------------------------------
