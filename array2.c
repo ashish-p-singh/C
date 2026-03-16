8ac568a913733e9e846e930936b5d1c2aab45a2a
Ashish Pratap Singh <ashishpratap.singh@outlook.com>
2026-03-16 09:46:38 +0530

    Add anagram checking implementation in array2.c

--- array2.c
+++ array2.c
@@ -59,4 +59,27 @@
 //     return 0;
 // }
 //----------------------------------------------
+#include <stdio.h>
+int main(int argc, char const *argv[])
+{
+    int arr[3]={'m','u','g'};
+    int arr2[3]={'g','u','m'};
+    int check=0;
+    for (int i=0;i<3;i++){
+        for (int j=0;j<3;j++){
+        if(arr[i]==arr2[j]){
+            check++;
+            break;
+        }
+    }
+}
+    if(check==3){
+        printf("Anagram");
+    }
+    else{
+        printf("Not an Anagram");
+    }
+
+    return 0;
+}
 
