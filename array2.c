d52e6bd1245c25cd3f252424d89ecc1782d1f6aa
Ashish Pratap Singh <ashishpratap.singh@outlook.com>
2026-03-27 14:33:28 +0530

    Refactor array2.c and tempCodeRunnerFile.c to remove anagram checking code and implement a matrix printing function

--- array2.c
+++ array2.c
@@ -112,29 +112,43 @@
 // printf("%d",b);
 // }
 //-------------------------------Anagram of string-------------------------------------
-#include <stdio.h>
-#include <string.h>
-void anagram(char str1[],char str2[]){
-    int count[256]={0};
-    int n1=strlen(str1);
-    int n2=strlen(str2);
-    for(int i=0;i<n1;i++){
-        count[str1[i]]++;
-        count[str2[i]]--;
-    }
-    for (int i=0;i<256;i++){
-        if(count[i]!=0){
-            printf("not an Anagram");
-            return ;
-        }
-    }
-    printf("Anagaram");
+// #include <stdio.h>
+// #include <string.h>
+// void anagram(char str1[],char str2[]){
+//     int count[256]={0};
+//     int n1=strlen(str1);
+//     int n2=strlen(str2);
+//     for(int i=0;i<n1;i++){
+//         count[str1[i]]++;
+//         count[str2[i]]--;
+//     }
+//     for (int i=0;i<256;i++){
+//         if(count[i]!=0){
+//             printf("not an Anagram");
+//             return ;
+//         }
+//     }
+//     printf("Anagaram");
     
-}
-int main()
-{
-    char str1[]="dmm";
-    char str2[]="mdm";
-    anagram(str1,str2);
-    return 0;
+// }
+// int main()
+// {
+//     char str1[]="dmm";
+//     char str2[]="mdm";
+//     anagram(str1,str2);
+//     return 0;
+// }
+//------------------------------------------------------
+#include <stdio.h>
+int main(){
+    int mat[4][4]={{2,3,4,5},
+                    {4,5,6,7},
+                    {6,7,8,9},
+                    {7,8,9,0}};
+    for (int i=0;i<4;i++){
+        for (int j=0;j<4;j++){
+            printf("%d ",mat[i][j]);
+        }
+        printf("\n");
+    }
 }
--- tempCodeRunnerFile.c
+++ tempCodeRunnerFile.c
@@ -1,22 +1,26 @@
 #include <stdio.h>
-// int main()
-// {
-//     int arr[3]={'m','u','g'};
-//     int arr2[3]={'g','u','m'};
-//     int check=0;
-//     for (int i=0;i<3;i++){
-//         for (int j=0;j<3;j++){
-//         if(arr[i]==arr2[j]){
-//             check++;
-//             break;
+// #include <string.h>
+// void anagram(char str1[],char str2[]){
+//     int count[256]={0};
+//     int n1=strlen(str1);
+//     int n2=strlen(str2);
+//     for(int i=0;i<n1;i++){
+//         count[str1[i]]++;
+//         count[str2[i]]--;
+//     }
+//     for (int i=0;i<256;i++){
+//         if(count[i]!=0){
+//             printf("not an Anagram");
+//             return ;
 //         }
 //     }
+//     printf("Anagaram");
+    
 // }
-//     if(check==3){
-//         printf("Anagram");
-//     }
-//     else{
-//         printf("Not an Anagram");
-//     }
+// int main()
+// {
+//     char str1[]="dmm";
+//     char str2[]="mdm";
+//     anagram(str1,str2);
 //     return 0;
-//  }
+// }
